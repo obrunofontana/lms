ac5b078ff010aa7f0c24f59ed9268a5a4929d2fa
Bruno Fontana <obrunofontana@gmail.com>
2023-09-29 09:47:57 -0300

    feat: implements list of routes available on navbar

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useAuth, UserButton } from '@clerk/nextjs';
+import { LogOut } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { Button } from '@/components/ui/button';
+import { isTeacher } from '@/lib/teacher';
+
+import { SearchInput } from './search-input';
+
+export const NavbarRoutes = () => {
+  const { userId } = useAuth();
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith('/teacher');
+  const isPlayerPage = pathname?.includes('/chapter');
+  const isCoursePage = pathname?.includes('/courses');
+  const isSearchPage = pathname === '/search';
+
+  return (
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Sair
+            </Button>
+          </Link>
+        ) : isTeacher(userId) ? (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Modo professor
+            </Button>
+          </Link>
+        ) : null}
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </>
+  );
+};
